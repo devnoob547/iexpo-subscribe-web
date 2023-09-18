6959ef0ce3f2e581c20e3c98bb2f04861f21f6d9
devnoob547 <danielalvespessoa3006@gmail.com>
2023-09-18 12:07:10 -0300

    add eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,62 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true,
+    'node': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'prettier',
+  ],
+  'overrides': [
+    {
+      'env': {
+        'node': true
+      },
+      'files': [
+        '.eslintrc.{js,cjs}'
+      ],
+      'parserOptions': {
+        'sourceType': 'script'
+      }
+    }
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'plugins': [
+    '@typescript-eslint',
+    'react',
+    'prettier'
+  ],
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'windows'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ],
+    'prettier/prettier': [
+      'error',
+      {
+        'singleQuote': true,
+        'endOfLine': 'crlf'
+      }
+    ],
+    'react/react-in-jsx-scope': 'off'
+  }
+};
