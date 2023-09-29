20edaee507c09a065cf88a5347ff3c1660e69b06
devnoob547 <danielalvespessoa3006@gmail.com>
2023-09-29 20:49:11 -0300

    add check to not send form with undefined fields

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -9,36 +9,43 @@ import {
   AlertDialogHeader,
   AlertDialogTrigger,
 } from './ui/alert-dialog';
-import { CheckCircle, Loader2, XCircle, User2, Mail } from 'lucide-react';
+import { CheckCircle, Loader2, User2, Mail } from 'lucide-react';
 import { useForm } from 'react-hook-form';
+import axios from 'axios';
 
-interface InputTypes {
+interface EnrollmentType {
   name: string;
   email: string;
 }
 
 export function Form() {
-  const [isRegister, setIsRegister] = useState<boolean | 'progress'>(false);
+  const [isRegister, setIsRegister] = useState<boolean>(false);
   const [name, setName] = useState<string>();
   const [email, setEmail] = useState<string>();
 
-  const { register, handleSubmit } = useForm<InputTypes>();
+  const { register, handleSubmit } = useForm<EnrollmentType>();
 
-  function handleClick({ name, email }: InputTypes) {
+  async function handleClick({ name, email }: EnrollmentType) {
+    console.log(name, email);
+
     if (!name && !email) {
       setIsRegister(false);
     }
 
-    setIsRegister('progress');
-    setName(name);
-    setEmail(email);
+    const { data } = await axios.post('http://localhost:5000/enrollment', {
+      name,
+      email,
+    });
 
-    setTimeout(() => setIsRegister(true), 2000);
+    setIsRegister(false);
+    setName(data.name);
+    setEmail(data.email);
+    setIsRegister(true);
   }
 
   return (
     <form
-      action=""
+      action="post"
       className="flex flex-col justify-center gap-4"
       onSubmit={handleSubmit(handleClick)}
     >
@@ -47,6 +54,7 @@ export function Form() {
         required
         type="text"
         placeholder="Digite seu nome"
+        onChange={(e) => setName(e.target.value)}
         className="border-[1px] text-base border-muted-foreground/20 shadow-sm shadow-muted-foreground/20 rounded-md px-4 py-2 outline-none focus:border-muted-foreground/40 focus:shadow-muted-foreground/40"
       />
       <input
@@ -54,31 +62,21 @@ export function Form() {
         required
         type="email"
         placeholder="Digite seu email"
+        onChange={(e) => setEmail(e.target.value)}
         className="border-[1px] text-base border-muted-foreground/20 shadow-sm shadow-muted-foreground/20 rounded-md px-4 py-2 outline-none focus:border-muted-foreground/40 focus:shadow-muted-foreground/40"
       />
       <div className="flex flex-col justify-center items-center">
         <AlertDialog>
           <AlertDialogTrigger
             type="submit"
-            className="bg-primary py-2 w-full text-white rounded-md shadow-primary/40 shadow-md"
+            className="bg-primary py-2 w-full text-white rounded-md shadow-primary/40 shadow-md disabled:bg-primary/50 disabled:cursor-not-allowed transition-all duration-200"
+            disabled={name && email ? false : true}
           >
             Fazer Inscrição!
           </AlertDialogTrigger>
           <AlertDialogContent>
             <AlertDialogHeader className="flex justify-center items-center">
-              {isRegister === 'progress' ? (
-                <>
-                  <AlertDialogDescription className="flex items-center gap-2 text-muted-foreground/60 p-2">
-                    Concluindo inscrição
-                    <Loader2 className="animate-spin" />
-                  </AlertDialogDescription>
-                  <AlertDialogFooter>
-                    <AlertDialogAction disabled className="w-full">
-                      Concluir
-                    </AlertDialogAction>
-                  </AlertDialogFooter>
-                </>
-              ) : isRegister ? (
+              {isRegister ? (
                 <>
                   <AlertDialogDescription className="flex flex-col items-center gap-2 text-green-500 p-2">
                     <div className="flex items-center gap-2">
@@ -104,13 +102,13 @@ export function Form() {
                 </>
               ) : (
                 <>
-                  <AlertDialogDescription className="flex items-center gap-2 text-red-500 p-2">
-                    Você não preencheu seus dados!
-                    <XCircle />
+                  <AlertDialogDescription className="flex items-center gap-2 text-muted-foreground/60 p-2">
+                    Concluindo inscrição
+                    <Loader2 className="animate-spin" />
                   </AlertDialogDescription>
                   <AlertDialogFooter>
-                    <AlertDialogAction className="w-full">
-                      Preencher
+                    <AlertDialogAction disabled className="w-full">
+                      Concluir
                     </AlertDialogAction>
                   </AlertDialogFooter>
                 </>
